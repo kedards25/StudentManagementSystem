9033c2ed27615d033301b779e10a44724ae34415
kedards25 <kedards25@gmail.com>
2020-02-14 19:57:34 +0530

    QR Refresh version restored

--- StudentAttendanceSystem/src/main/java/com/learning/niit_project/services/IFacultyDAO.java
+++
@@ -1,9 +0,0 @@
-package com.learning.niit_project.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.learning.niit_project.models.FacultyModel;
-
-public interface IFacultyDAO extends CrudRepository<FacultyModel, Integer> {
-
-}
--- StudentAttendanceSystem/src/main/java/com/learning/niit_project/services/I_QRdao.java
+++
@@ -1,9 +0,0 @@
-package com.learning.niit_project.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.learning.niit_project.models.QR_Details;
-
-public interface I_QRdao extends CrudRepository<QR_Details,	Integer> {
-
-}
