994267d089d709a07d444cec8efcafa3f2546b32
kedards25 <kedards25@gmail.com>
2020-02-15 01:58:28 +0530

    Dummy Data for student Model added

--- StudentAttendanceSystem/src/main/java/com/learning/niit_project/controllers/StudentController.java
+++ StudentAttendanceSystem/src/main/java/com/learning/niit_project/controllers/StudentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,41 +25,45 @@ public class StudentController {
 
 	@Autowired
 	IStudentDAO istdDAO;
-	
 
-	
-	
-	@GetMapping(path="/students",produces = {"application/json"})
+	@RequestMapping("/RegisterStudent")
+	public String regFac()
+	{
+		StudentModel model=new StudentModel(103, "Balaji", "App Engine");
+		istdDAO.save(model);
+		return "faculty-reg";
+		
+	}
+
+	@GetMapping(path = "/students", produces = { "application/json" })
 	@ResponseBody
 	public List<StudentModel> getAllUsers() {
-		//List<UserModel> model = iuserDao.findAll();
-		//System.out.println("Details: "+model.size());
+		// List<UserModel> model = iuserDao.findAll();
+		// System.out.println("Details: "+model.size());
 		return istdDAO.findAll();
 	}
-	
-	@PostMapping(path="/students",consumes = {"application/json"})
+
+	@PostMapping(path = "/students", consumes = { "application/json" })
 	public ResponseEntity addDetails(@RequestBody StudentModel model) {
 		istdDAO.save(model);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
-	
-	@PutMapping(path="/students",consumes = {"application/json"})
+
+	@PutMapping(path = "/students", consumes = { "application/json" })
 	public ResponseEntity updateDetails(@RequestBody StudentModel model) {
 		istdDAO.save(model);
 		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
-	
-	@DeleteMapping(path="/students/{id}",consumes = {"application/json"})
+
+	@DeleteMapping(path = "/students/{id}", consumes = { "application/json" })
 	public ResponseEntity deleteDetails(@PathVariable("id") int id) {
 		istdDAO.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.GONE);
 	}
-	
-	@GetMapping(path="/students/{id}",consumes = {"application/json"})
+
+	@GetMapping(path = "/students/{id}", consumes = { "application/json" })
 	public Optional<StudentModel> studentById(@PathVariable("id") int id) {
 		return istdDAO.findById(id);
 	}
-	
-	
-	
+
 }
